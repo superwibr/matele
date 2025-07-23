88b5ca5767ab05c3f89c69794158306281c4e326
superwibr <superwibr@gmail.com>
2025-07-23 13:04:29 -0400

    rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import { Renderer } from "../lib/Renderer.js";
 import { Simulation } from "../lib/Simulation.js";
+import { SquareLJP } from "./particles/SquareLJP.js";
 import { SquareWell } from "./particles/SquareWell.js";
 
 const sim = new Simulation();
 
+const canvas = document.querySelector("canvas");
+const ren = new Renderer(sim, canvas, {
+	gravity(v) {
+		return v > 0 ? [v, 0, 0] : [0, 0, Math.abs(v*5)];
+	}
+});
+
+ren.start();
+
 window.SquareWell = SquareWell;
+window.SquareLJP = SquareLJP;
 window.sim = sim;
+window.ren = ren;
